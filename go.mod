ba41bfbc5c63e62854bcfc14c2e6c5f8e4861001
zachdehooge <zacharydehooge@gmail.com>
2026-07-26 11:59:35 -0400

    Stage 1 of adding in cli flags for passing files instead of a SHA256

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/zachdehooge/VTScan
 
 go 1.23.0
 
-require (
-	github.com/antonholmquist/jason v1.0.0
-	github.com/joho/godotenv v1.5.1
-)
+require github.com/antonholmquist/jason v1.0.0
--- vtscan.go
+++ vtscan.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"crypto/sha256"
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
 
@@ -10,6 +13,66 @@ import (
 )
 
 // 1445f8dc16bf7f0e1c7b3d16bee14ef83e6170ab00a2381d509051c64617fbfd / proquota.exe
+func determineFileSHA256(file string) []byte {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%x", h.Sum(nil))
+	return h.Sum(nil)
+}
+
+func SHA256Scan(sha256 string) {
+
+	apikey := os.Getenv("VTAPIKEY")
+
+	url := "https://www.virustotal.com/api/v3/files/" + sha256
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("x-apikey", apikey)
+
+	res, _ := http.DefaultClient.Do(req)
+
+	v, _ := jason.NewObjectFromReader(res.Body)
+
+	threatLabel, _ := v.GetString("data", "attributes", "popular_threat_classification", "suggested_threat_label")
+
+	malCat, _ := v.GetInt64("data", "attributes", "last_analysis_stats", "malicious")
+	susCat, _ := v.GetInt64("data", "attributes", "last_analysis_stats", "suspicious")
+	undetectedCat, _ := v.GetInt64("data", "attributes", "last_analysis_stats", "undetected")
+	harmlessCat, _ := v.GetInt64("data", "attributes", "last_analysis_stats", "harmless")
+	timeoutCat, _ := v.GetInt64("data", "attributes", "last_analysis_stats", "timeout")
+	confirmedtimeoutCat, _ := v.GetInt64("data", "attributes", "last_analysis_stats", "confirmed-timeout")
+	failureCat, _ := v.GetInt64("data", "attributes", "last_analysis_stats", "failure")
+	unsupportCat, _ := v.GetInt64("data", "attributes", "last_analysis_stats", "type-unsupported")
+	repCat, _ := v.GetInt64("data", "attributes", "reputation")
+
+	fmt.Println("\nThreat Label: ", threatLabel)
+	fmt.Println("____________________________________")
+	fmt.Println("\nMalicious: ", malCat)
+	fmt.Println("Suspicious: ", susCat)
+	fmt.Println("Undetected: ", undetectedCat)
+	fmt.Println("Harmless: ", harmlessCat)
+	fmt.Println("Timeout: ", timeoutCat)
+	fmt.Println("Confirmed Timeout: ", confirmedtimeoutCat)
+	fmt.Println("Failure: ", failureCat)
+	fmt.Println("Unsupported: ", unsupportCat)
+	fmt.Println("____________________________________")
+
+	fmt.Println("\nReputation: ", repCat)
+
+	defer res.Body.Close()
+}
+
 func fileScan(sha256 string) {
 
 	apikey := os.Getenv("VTAPIKEY")
@@ -36,9 +99,7 @@ func fileScan(sha256 string) {
 	failureCat, _ := v.GetInt64("data", "attributes", "last_analysis_stats", "failure")
 	unsupportCat, _ := v.GetInt64("data", "attributes", "last_analysis_stats", "type-unsupported")
 	repCat, _ := v.GetInt64("data", "attributes", "reputation")
-	//popName, _ := v.GetStringArray("data", "attributes", "popular_threat_classification", "popular_threat_name", "value")
 
-	//fmt.Println("\nThreat Names: ", popName)
 	fmt.Println("\nThreat Label: ", threatLabel)
 	fmt.Println("____________________________________")
 	fmt.Println("\nMalicious: ", malCat)
@@ -58,17 +119,22 @@ func fileScan(sha256 string) {
 
 func main() {
 	var sha256Flag = flag.String("sha256", "", "SHA256 to check")
+	var fileFlag = flag.String("file", "", "File to check")
 
 	flag.Parse()
 
 	sha256F := *sha256Flag
+	fileF := *fileFlag
 
 	if sha256F != "" {
-		fileScan(sha256F)
+		SHA256Scan(sha256F)
+	} else if fileF != "" {
+		fileSHA256 := determineFileSHA256(fileF)
+		fileScan(string(fileSHA256))
 	} else {
 		var i string
 		fmt.Print("\nSHA256 of Sample: ")
 		fmt.Scan(&i)
-		fileScan(i)
+		SHA256Scan(i)
 	}
 }
